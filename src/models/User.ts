848b4145c6f4058c1c36437e8e8921ac162764aa
Aditya Pranjal <srivastavatubbu@gmail.com>
2026-05-23 01:36:49 +0530

    Create user database model

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default User;
